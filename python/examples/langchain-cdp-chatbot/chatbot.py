6697435bcaadc7ebd03f2ec2606e0131ee11250d
khanhtiet <khanh.tiet.6001@gmail.com>
2025-04-27 18:27:04 +0700

    update

--- python/examples/langchain-cdp-chatbot/chatbot.py
+++ python/examples/langchain-cdp-chatbot/chatbot.py
@@ -17,7 +17,8 @@ from coinbase_agentkit import (
     weth_action_provider,
 )
 
-from coinbase_agentkit.action_providers.pond import pond_action_provider
+from coinbase_agentkit.action_providers.pond.pond_action_provider import pond_action_provider
+
 from coinbase_agentkit_langchain import get_langchain_tools
 from dotenv import load_dotenv
 from langchain_core.messages import HumanMessage
